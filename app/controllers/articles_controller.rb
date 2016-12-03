65c5baf2ab14c7698d3d4380ed540e5842786125
Ziv Zamechek <ziv921@yahoo.com>
2016-12-03 17:04:10 -0500

    Create new template, action and route

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,25 @@
+class ArticlesController < ApplicationController
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:success] = "Article was successfully created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+
+  end
+
+  private 
+
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
+end
